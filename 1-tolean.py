1a1b86a52628308436ff2df40384bff39098811a
Alex Keizer <alex@keizer.dev>
2023-10-06 17:31:12 +0100

    Change lean generation preamble

--- 1-tolean.py
+++ 1-tolean.py
@@ -587,9 +587,16 @@ def print_as_lean(opt):
   return out;
 
 
-LEAN_PREAMBLE= """
+LEAN_PREAMBLE = """
+import SSA.Projects.InstCombine.Tactic
 import SSA.Projects.InstCombine.LLVM.EDSL
 open MLIR AST
+
+abbrev ICom.Refinement (src tgt : Com (φ:=0) Γ t) (h : Goedel.toType t = Option α := by rfl) : Prop :=
+  ∀ Γv, Bitvec.Refinement (h ▸ src.denote Γv) (h ▸ tgt.denote Γv)
+
+infixr:90 " ⊑ "  => ICom.Refinement
+
 """
 
 class Statistics:
